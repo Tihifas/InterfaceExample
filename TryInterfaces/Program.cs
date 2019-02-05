c28b56faa6e506515abdc46128359a82637ac87b
Torstein Bratberg <tdbratberg@gmail.com>
2019-02-05 11:36:25 +0100

    Add project files.

---
+++ TryInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
